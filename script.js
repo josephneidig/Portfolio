99f049f688ee42d37a171054916245656d9e4a63
Joseph Neidig <joseph.neidig@ucf.edu>
2022-03-25 12:46:40 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var i = 0;
+function toggleNav()
+{
+  if (i == 0)
+  {
+    document.getElementById("nav").style.height = "100%";
+    document.getElementById("nav").style.padding = "5em 0 0 0";
+    //document.getElementById("notnav-wrapper").style.marginLeft = "250px";
+    i = 1;
+  }
+  else
+  {
+    document.getElementById("nav").style.height = "0%";
+    document.getElementById("nav").style.padding = "0";
+    //document.getElementById("notnav-wrapper").style.marginLeft= "0";
+    i = 0;
+  }
+}
